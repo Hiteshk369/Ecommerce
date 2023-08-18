3ee2204b891663b032142686ac381d3839b48186
Turbobot <turbobot@vercel.com>
2023-08-18 17:53:24 +0530

    feat(create-turbo): apply official-starter transform

--- frontend/src/libs/queryFunctions.ts
+++
@@ -1,7 +0,0 @@
-import { queryClient } from "../App";
-
-export const handleRefetchCartItems = () =>
-  queryClient.refetchQueries(["cartItems"]);
-
-export const handleRefetchByCategory = () =>
-  queryClient.refetchQueries(["productByCategory"]);
--- frontend/src/pages/error/index.tsx
+++
@@ -1,5 +0,0 @@
-const Error404 = () => {
-  return <div className=" text-red-700">Error404</div>;
-};
-
-export default Error404;
