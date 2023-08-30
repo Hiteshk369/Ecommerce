61e561e286cab1499134c6e0c65428a945698214
Hiteshk369 <parchahiteshkumar123@gmail.com>
2023-08-31 00:46:57 +0530

    admin overview

--- apps/admin/app/(site)/page.tsx
+++ apps/admin/app/(site)/page.tsx
@@ -1,5 +1,7 @@
+import OverviewChart from "../../components/OverviewChart";
 import CardContainer from "../../components/CardContainer";
 import React from "react";
+import { IndianRupee, Plus, User } from "lucide-react";
 
 const Home = () => {
   return (
@@ -14,6 +16,49 @@ const Home = () => {
       </div>
       <div className="w-full h-[25%]">
         <CardContainer />
+      </div>
+      <div className="h-[60%] w-full mt-5 flex gap-4">
+        <div className="w-[60%] h-full flex items-center justify-center border border-neutral-600 rounded-lg p-4">
+          <OverviewChart />
+        </div>
+        <div className="w-[40%] h-full border border-neutral-600 rounded-lg p-4">
+          <p className="text-lg text-pearl font-medium">Recent sales</p>
+          <div className="flex flex-col gap-4 pt-5">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-4">
+                <div className="w-10 h-10 rounded-full bg-pearl flex items-center justify-center">
+                  <User size={25} className="text-black" />
+                </div>
+                <div className="flex flex-col text-pearl">
+                  <p className="font-medium">Hitesh</p>
+                  <p className="font-normal text-subGray">hitesh@gmail.com</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <Plus size={18} className="text-pearl" />
+                <IndianRupee size={20} className="text-pearl ml-1" />
+                <p className="text-xl text-pearl">76000</p>
+              </div>
+            </div>
+            {/* another */}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-4">
+                <div className="w-10 h-10 rounded-full bg-pearl flex items-center justify-center">
+                  <User size={25} className="text-black" />
+                </div>
+                <div className="flex flex-col text-pearl">
+                  <p className="font-medium">Hitesh</p>
+                  <p className="font-normal text-subGray">hitesh@gmail.com</p>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <Plus size={18} className="text-pearl" />
+                <IndianRupee size={20} className="text-pearl ml-1" />
+                <p className="text-xl text-pearl">76000</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- apps/admin/components/CardContainer/index.tsx
+++ apps/admin/components/CardContainer/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import {
   Card,
   CardContent,
@@ -5,35 +6,62 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
-import { IndianRupee, MoveUpRight } from "lucide-react";
+import { IndianRupee, MoveUpRight, Users, ShoppingBag } from "lucide-react";
 
 const CardContainer = () => {
+  const cardData = useMemo(
+    () => [
+      {
+        id: 1,
+        title: "Total Sales",
+        Icon: IndianRupee,
+        content: "10,89,065.67",
+        footerContent: "20.9%",
+      },
+      {
+        id: 2,
+        title: "Total Orders",
+        Icon: ShoppingBag,
+        content: "9907",
+        footerContent: "14.1%",
+      },
+      {
+        id: 3,
+        title: "Total Users",
+        Icon: Users,
+        content: "1679",
+        footerContent: "29.4%",
+      },
+    ],
+    []
+  );
   return (
     <div className="h-full w-full flex justify-between">
-      <Card className="bg-cardColor w-[30%] border-neutral-700">
-        <CardHeader>
-          <CardTitle className="text-pearl flex items-center justify-between gap-3">
-            <p className="text-lg tracking-wide font-normal">Total Sales</p>
-            <IndianRupee className="w-7 h-7 p-[5px] bg-[#151515] rounded-full flex items-center justify-center text-main" />
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="flex items-center gap-2">
-            <IndianRupee size={25} className="text-pearl" />
-            <p className="text-pearl text-3xl font-bold">10,89,065.67</p>
-          </div>
-        </CardContent>
-      </Card>
-      <Card className="bg-cardColor w-[30%] border-neutral-700">
-        <CardHeader>
-          <CardTitle className="text-pearl">Total Sales</CardTitle>
-        </CardHeader>
-      </Card>
-      <Card className="bg-cardColor w-[30%] border-neutral-700">
-        <CardHeader>
-          <CardTitle className="text-pearl">Total Sales</CardTitle>
-        </CardHeader>
-      </Card>
+      {cardData.map((card) => (
+        <Card key={card.id} className="bg-cardColor w-[30%] border-neutral-700">
+          <CardHeader>
+            <CardTitle className="text-pearl flex items-center justify-between gap-3">
+              <p className="text-lg tracking-wide font-normal">{card.title}</p>
+              <card.Icon size={20} className=" text-main" />
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="flex items-center gap-2">
+              {card.id === 1 && (
+                <IndianRupee size={25} className="text-pearl" />
+              )}
+
+              <p className="text-pearl text-3xl font-bold">{card.content}</p>
+            </div>
+            <div className="flex items-center pt-3 gap-2">
+              <MoveUpRight size={15} className="text-main" />
+              <p className="text-main text-sm font-normal">
+                {card.footerContent}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 };
