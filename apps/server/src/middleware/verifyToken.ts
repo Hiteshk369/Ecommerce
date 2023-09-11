6a5d71725189b91af523b2d1ad3a9a7858c3547a
Hiteshk369 <parchahiteshkumar123@gmail.com>
2023-09-11 23:47:12 +0530

    jwt err fix

--- apps/server/src/middleware/verifyToken.ts
+++ apps/server/src/middleware/verifyToken.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import httpError from "http-errors";
-import jwt from "jsonwebtoken";
+import jwt, { VerifyErrors } from "jsonwebtoken";
 import { publicKeyAccessToken } from "../helpers/jwt";
 import { Schema } from "mongoose";
 
 export interface IRequest extends Request {
   userId?: Schema.Types.ObjectId;
+}
+
+interface JWTPayload {
+  id: Schema.Types.ObjectId;
 }
 
 export const verifyToken = (
@@ -16,12 +20,19 @@ export const verifyToken = (
   if (!req.headers["authorization"]) return next(httpError.Unauthorized());
   const authHeader = req.headers["authorization"];
   const token = authHeader.split(" ")[1];
-  const payload = jwt.verify(token, publicKeyAccessToken) as {
-    id: Schema.Types.ObjectId;
-  };
-  if (!payload){
-    return next(httpError.Unauthorized());
-  }
-  req.userId = payload.id;
-  next();
+  jwt.verify(
+    token,
+    publicKeyAccessToken,
+    (err: VerifyErrors | null, decoded: any) => {
+      if (err) {
+        return next(httpError.Unauthorized("Token expired"));
+      } else {
+        const payload = decoded as JWTPayload;
+        if (payload && payload.id) {
+          req.userId = payload.id;
+          next();
+        }
+      }
+    }
+  );
 };
